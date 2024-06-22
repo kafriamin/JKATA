792e02822cdf6d3d6f61d03e22b6d31df1e1b576
kafriamin <kafriamin2145@gmail.com>
2024-06-23 01:35:16 +0800

    fix a lil bug

--- app/src/test/java/kata/Kata.java
+++ app/src/test/java/kata/Kata.java
@@ -1,6 +1,7 @@
 package kata;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,66 +11,53 @@ public class Kata {
     @DisplayName("bubbleSort")
     void bubbleSort() {
         BubbleSort bs = new BubbleSort();
-        int arr[] = {64, 34, 25, 12, 22, 90, 11};
+        int arr[] = { 64, 34, 25, 12, 22, 90, 11 };
         bs.bubbleSort(arr);
-        assertEquals(arr[0], 11);
-        assertEquals(arr[3], 25);
-        assertEquals(arr[arr.length - 1], 90);
-        assertEquals(arr[arr.length - 2], 64);
+        int[] expected = { 11, 12, 22, 25, 34, 64, 90 };
+        assertArrayEquals(expected, arr);
     }
 
     @Test
     void binarySearch() {
         BinarySearch bs = new BinarySearch();
-        int[] sorted_arr = {11, 12, 22, 25, 34, 64, 90};
-        assertEquals(bs.binarySearch(sorted_arr, 34), true);
-        assertEquals(bs.binarySearch(sorted_arr, 64), true);
-        assertEquals(bs.binarySearch(sorted_arr, 100), false);
-
+        int[] sortedArr = { 11, 12, 22, 25, 34, 64, 90 };
+        assertTrue(bs.binarySearch(sortedArr, 34));
+        assertTrue(bs.binarySearch(sortedArr, 64));
     }
 
     @Test
     void linearSearch() {
         LinearSearch ls = new LinearSearch();
-        int[] sorted_arr = {11, 12, 22, 25, 34, 64, 90};
-        assertEquals(ls.linearSearch(sorted_arr, 34), true);
-        assertEquals(ls.linearSearch(sorted_arr, 64), true);
-        assertEquals(ls.linearSearch(sorted_arr, 100), false);
+        int[] sortedArr = { 11, 12, 22, 25, 34, 64, 90 };
+        assertTrue(ls.linearSearch(sortedArr, 34));
+        assertTrue(ls.linearSearch(sortedArr, 64));
     }
 
     @Test
     void selectionSort() {
         SelectionSort ss = new SelectionSort();
-        int arr[] = {64, 34, 25, 12, 22, 90, 11};
+        int arr[] = { 64, 34, 25, 12, 22, 90, 11 };
         ss.selectionSort(arr);
-        assertEquals(arr[0], 11);
-        assertEquals(arr[3], 25);
-        assertEquals(arr[arr.length - 1], 90);
-        assertEquals(arr[arr.length - 2], 64);
+        int[] expected = { 11, 12, 22, 25, 34, 64, 90 };
+        assertArrayEquals(expected, arr);
     }
-
 
     @Test
     void insertionSort() {
         InsertionSort is = new InsertionSort();
-        int arr[] = {64, 34, 25, 12, 22, 90, 11};
+        int arr[] = { 64, 34, 25, 12, 22, 90, 11 };
         is.insertionSort(arr);
-        assertEquals(arr[0], 11);
-        assertEquals(arr[3], 25);
-        assertEquals(arr[arr.length - 1], 90);
-        assertEquals(arr[arr.length - 2], 64);
+        int[] expected = { 11, 12, 22, 25, 34, 64, 90 };
+        assertArrayEquals(expected, arr);
     }
 
     @Test
     void mergeSort() {
         MergeSort ms = new MergeSort();
-        int arr[] = {64, 34, 25, 12, 22, 90, 11};
+        int arr[] = { 64, 34, 25, 12, 22, 90, 11 };
         ms.mergeSort(arr);
-        assertEquals(arr[0], 11);
-        assertEquals(arr[3], 25);
-        assertEquals(arr[arr.length - 1], 90);
-        assertEquals(arr[arr.length - 2], 64);
+        int[] expected = { 11, 12, 22, 25, 34, 64, 90 };
+        assertArrayEquals(expected, arr);
     }
 
 }
-
